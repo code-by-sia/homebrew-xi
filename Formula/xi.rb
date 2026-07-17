9efb4ba21a34c7eb645815c9bb65aca56ba6e4e4
xi tap sync <9437465+code-by-sia@users.noreply.github.com>
2026-07-17 00:19:27 +0000

    xi 0.1.0 (auto-sync)

--- Formula/xi.rb
+++ Formula/xi.rb
@@ -10,28 +10,28 @@
 class Xi < Formula
   desc "The Ξ (Xi) programming language toolchain (compiler + REPL)"
   homepage "https://github.com/code-by-sia/xi"
-  version "0.0.99"
+  version "0.1.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/code-by-sia/xi/releases/download/v0.0.99/xi-v0.0.99-macos-arm64.tar.gz"
-      sha256 "4e8ed48bf3fb0adcad7fcd365d3d508bffa9c921779497f5a59f863f942056c3"
+      url "https://github.com/code-by-sia/xi/releases/download/v0.1.0/xi-v0.1.0-macos-arm64.tar.gz"
+      sha256 "a2a47193e3f161827d2181a49848f9053100265d92b52cd52c55ab183e369447"
     end
     on_intel do
-      url "https://github.com/code-by-sia/xi/releases/download/v0.0.99/xi-v0.0.99-macos-x86_64.tar.gz"
-      sha256 "f8265e030e5d6e2c25fc95e803d9b848e6d6d32e49e54b24f9bf4a85d8080b96"
+      url "https://github.com/code-by-sia/xi/releases/download/v0.1.0/xi-v0.1.0-macos-x86_64.tar.gz"
+      sha256 "20bc925f368d2e8bd76085379e757ea24d7e4fbc7ee0b8d736ec839113187b44"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/code-by-sia/xi/releases/download/v0.0.99/xi-v0.0.99-linux-arm64.tar.gz"
-      sha256 "55f0c71f204ce4e19231d90af03552ecbcc098cbaaa4e7c62874f7f8908caa8c"
+      url "https://github.com/code-by-sia/xi/releases/download/v0.1.0/xi-v0.1.0-linux-arm64.tar.gz"
+      sha256 "1d3a731d31a703d7cebed04bbbadec739b8b33fdfc2debc95ef990316a0b08a6"
     end
     on_intel do
-      url "https://github.com/code-by-sia/xi/releases/download/v0.0.99/xi-v0.0.99-linux-x86_64.tar.gz"
-      sha256 "3514bfbc1dd8021608e57fb1591ad34cb21f8b572f56f7d848fc514a975b09e7"
+      url "https://github.com/code-by-sia/xi/releases/download/v0.1.0/xi-v0.1.0-linux-x86_64.tar.gz"
+      sha256 "5d2b9afa01d36ec2da03c3417b48cbd0f4503fb95060ebe609cd88400b96bb7b"
     end
   end
 
